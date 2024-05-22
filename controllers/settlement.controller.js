0ea66246656fde4e792338fcc00fa74a22383397
akshay <akshay@wemotiveforge.com>
2024-05-22 10:55:18 +0530

    fix: import of settlement

--- controllers/settlement.controller.js
+++ controllers/settlement.controller.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 const createSettlementDetails = async (req, res) => {
     const { settlementType, accountNo, bankName, branchName, orderId } = req.body;
     try {
-        const newSettlementDetails = await settlementDetailsService.createSettlementDetails(settlementType, accountNo, bankName, branchName, orderId);
+        const newSettlementDetails = await settlementService.createSettlementDetails(settlementType, accountNo, bankName, branchName, orderId);
         res.json(newSettlementDetails);
     } catch (err) {
         console.error('Error creating settlement details', err);
@@ -16,7 +16,7 @@ const createSettlementDetails = async (req, res) => {
 const getAllSettlementDetails = async (req, res) => {
     try {
         const { limit, offset, settlementType, accountNo, bankName, branchName, startTime, endTime } = req.query;
-        const settlementDetails = await settlementDetailsService.getAllSettlementDetails(limit, offset,settlementType, accountNo, bankName, branchName, startTime, endTime);
+        const settlementDetails = await settlementService.getAllSettlementDetails(limit, offset,settlementType, accountNo, bankName, branchName, startTime, endTime);
         res.json(settlementDetails);
     } catch (err) {
         console.error('Error getting settlement details', err);
@@ -27,7 +27,7 @@ const getAllSettlementDetails = async (req, res) => {
 const getSettlementById = async(req, res)=> {
     try {
       const { id } = req.params;
-      const settlement = await settlementDetailsService.getSettlementById(id);
+      const settlement = await settlementService.getSettlementById(id);
       res.json(settlement);
     } catch(err){
       res.status(500).json({ error: 'Internal Server Error'})
@@ -37,7 +37,7 @@ const getSettlementById = async(req, res)=> {
 const exportToExcel = async (req, res) => {
     const filePath = 'settlementDetails.xlsx';
     try {
-        await settlementDetailsService.exportToExcel(filePath);
+        await settlementService.exportToExcel(filePath);
         res.download(filePath, (err) => {
             if (err) {
                 throw new Error('Error downloading file');
