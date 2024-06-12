5b32d68b0a7b9bd28ab448259e260be094110695
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-06-12 12:05:35 +0530

    ADD : Added exportFinancialsToExcel route

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -9,5 +9,6 @@ router.get('/orders', orderController.getAllOrders);
 router.get('/orders/:id', orderController.getOrderById);
 router.get('/orders/state/count', orderController.getOrderStateCounts);
 router.get('/orders/download/xlsx', orderController.exportToExcel);
+router.get('/financials/donwload/xlsx', orderController.exportFinancialsToExcel)
 
 export default router;
