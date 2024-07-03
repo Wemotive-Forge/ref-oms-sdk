5443634e82e04692f63bcefe82717a3357bacfdb
akshay <akshay@wemotiveforge.com>
2024-07-03 16:54:05 +0530

    chg: route signature

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -7,59 +7,59 @@ const searchController = new SearchController();
 
 // search
 router.get(
-    '/v2/search',  searchController.search,
+    '/search',  searchController.search,
 );
 
 // search
 router.get(
-    '/v2/search/global/items',  searchController.globalSearchItems,
+    '/search/global/items',  searchController.globalSearchItems,
 );
 
 // get item details
 router.get(
-    '/v2/provider-details',searchController.getProvideDetails,
+    '/provider-details',searchController.getProvideDetails,
 );
 // get item details
 router.get(
-    '/v2/location-details',searchController.getLocationDetails,
+    '/location-details',searchController.getLocationDetails,
 );
 
 // get item details
 router.get(
-    '/v2/item-details',searchController.getItemDetails,
+    '/item-details',searchController.getItemDetails,
 );
 
 router.get(
-    '/v2/attributes',  searchController.getAttributes,
+    '/attributes',  searchController.getAttributes,
 );
 
 router.get(
-    '/v2/locations',  searchController.getLocations,
+    '/locations',  searchController.getLocations,
 );
 
 // get item attributes values
 router.get(
-    '/v2/attributeValues',  searchController.getAttributesValues,
+    '/attributeValues',  searchController.getAttributesValues,
 );
 
 // get providers
 router.get(
-    '/v2/providers',  searchController.getProviders,
+    '/providers',  searchController.getProviders,
 );
 
 // get providers
 router.get(
-    '/v2/search/global/providers',  searchController.getGlobalProviders,
+    '/search/global/providers',  searchController.getGlobalProviders,
 );
 
 // get custom menus
 router.get(
-    '/v2/custom-menus',  searchController.getCustomMenu,
+    '/custom-menus',  searchController.getCustomMenu,
 );
 
 // get offers
 router.get(
-    '/v2/offers',  searchController.getOffers,
+    '/offers',  searchController.getOffers,
 );
 
 export default router;
