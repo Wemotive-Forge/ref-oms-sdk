1964390f4a3b9457c2838340fea399b90539e270
Akshay Chavan <akshay@wemotiveforge.com>
2024-07-23 10:02:52 +0530

    fix: elastic env

--- database/elasticSearch.js
+++ database/elasticSearch.js
@@ -3,7 +3,7 @@ import loadEnvVariables from '../utils/envHelper.js';
 loadEnvVariables();
 
 const client = new Client({
-  node: "http://127.0.0.1:9200"
+  node: process.env.ELASTIC_SEARCH_URL
 });
 
 export default client;
